55a64b8c3571219c2889079103ae7cdbc37d0c7c
ajhoneslacubtan <ajjhones.lacubtan@g.msuiit.edu.ph>
2025-05-17 01:04:01 +0800

    Initial Commit

---
+++ pages/__init__.py
@@ -0,0 +1,17 @@
+# Copyright 2021-2024 Avaiga Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+from .root import root_page
+from .dashboard.dashboard import dashboard
+from .scenario_studio.scenario_studio import scenario_studio
+from .data_explorer.data_explorer import data_explorer
+from .model_insights.model_insights import model_insights
+from .run_history.run_history import run_history
